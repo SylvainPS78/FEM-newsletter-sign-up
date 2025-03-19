fb57e6998d08c1bf02f49597875b6c46281d0a83
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-19 13:44:11 +0700

    FEAT Add success message

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const form = document.getElementById("form");
 const submitBtn = document.getElementById("submit-btn");
 const userInput = document.getElementById("user-input");
 const errorMessage = document.getElementById("error-message");
+const mainPage = document.getElementById("main-page");
+const successPage = document.getElementById("success-page");
+const dismissBtn = document.getElementById("dismiss-btn");
+const confirmMsg = document.getElementById("confirm-msg");
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,14 +15,24 @@ const handleSubmit = (e) => {
 
 form.addEventListener('submit',handleSubmit);
 
-submitBtn.addEventListener("click", (e) => {
+submitBtn.addEventListener("click", () => {
     if (userInput.validity.typeMismatch || userInput.validity.valueMissing) {
         errorMessage.style.display = "block";
     }
     else {
         errorMessage.style.display = "none";
+        mainPage.style.display = "none";
+        confirmMsg.innerHTML = `A confirmation email has been sent to <b>${userInput.value}</b>. 
+        Please open it and click the button inside to confirm your subscription.`
+        successPage.style.display = "flex";
     }
 })
+
+dismissBtn.addEventListener("click",() => {
+    mainPage.style.display = "flex";
+    successPage.style.display = "none";
+    form.reset();}
+)
 
 
 
